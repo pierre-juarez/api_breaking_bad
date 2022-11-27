504d4239bd83f407d2cfbea07b33481b8d68fae0
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-11-27 00:24:08 -0500

    [add] HttpClient & getData

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Character } from 'src/app/interfaces/Character';
 
 @Component({
   selector: 'app-main-page',
@@ -7,7 +9,22 @@ import { Component } from '@angular/core';
   ]
 })
 export class MainPageComponent {
-  title = 'Whati'
+  characters: Character[] | undefined;
+
+  constructor(public http: HttpClient){
+    this.getData();
+  }
+
+  title = 'API Breaking Bad'
+  apiURL = 'https://breakingbadapi.com/api/characters'
+
+  async getData(){
+    await this.http
+      .get<any>(this.apiURL)
+      .subscribe((resp) => {
+        console.table(resp)
+      });
+  }
   
   filter(e: any){
     const value = e.target.value;
