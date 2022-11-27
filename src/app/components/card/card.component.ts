08a74c31b3eeeab95ad3a185062d1a811130aebf
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-11-27 02:09:32 -0500

    [add] Print character

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from 'src/app/interfaces/Character';
 
 @Component({
   selector: 'app-card',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   ]
 })
 export class CardComponent {
-
+  @Input() character!: Character
 }
